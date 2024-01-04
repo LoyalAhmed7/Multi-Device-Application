ca7b9ef441711e5e80d9004c8eaf0d7378a66973
Ahmed Raheem <114246808+LoyalAhmed7@users.noreply.github.com>
2024-01-04 20:46:57 +0400

    changes

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
+// Function to toggle the visibility of the navigation menu
 function openMenu() {
+    // Select the <nav> element in the document
     const nav = document.querySelector('nav');
+    
+    // Toggle the 'open' class on the <nav> element
     nav.classList.toggle('open');
 }
